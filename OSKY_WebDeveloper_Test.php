b81c6accd0ca5200188f3331a8107141aafc6af8
firdausjo08 <53815424+firdausjo08@users.noreply.github.com>
2021-06-25 11:27:26 +0800

    Add files via upload

---
+++ OSKY_WebDeveloper_Test.php
@@ -0,0 +1,114 @@
+<html>
+
+<head>
+    <style>
+        .fade-in-text {
+            display: inline-block;
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 10px;
+            color: black;
+            animation: fadeIn linear 7s;
+            -webkit-animation: fadeIn linear 7s;
+            -moz-animation: fadeIn linear 7s;
+            -o-animation: fadeIn linear 7s;
+            -ms-animation: fadeIn linear 7s;
+        }
+
+        @keyframes fadeIn {
+            0% {
+                opacity: 0;
+            }
+
+            100% {
+                opacity: 1;
+            }
+        }
+
+        @-moz-keyframes fadeIn {
+            0% {
+                opacity: 0;
+            }
+
+            100% {
+                opacity: 1;
+            }
+        }
+
+        @-webkit-keyframes fadeIn {
+            0% {
+                opacity: 0;
+            }
+
+            100% {
+                opacity: 1;
+            }
+        }
+
+        @-o-keyframes fadeIn {
+            0% {
+                opacity: 0;
+            }
+
+            100% {
+                opacity: 1;
+            }
+        }
+
+        @-ms-keyframes fadeIn {
+            0% {
+                opacity: 0;
+            }
+
+            100% {
+                opacity: 1;
+            }
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="fade-in-text">
+        <h2>Yankees' Rivera earns 600th career save (AP)</h2>
+        <p><a href="http://us.rd.yahoo.com/sports/rss/top/SIG=11jrnp014/*http%3A//sports.yahoo.com/mlb/recap?gid=310913112"><img src="http://d.yimg.com/a/p/sp/ap/05/thumbe.ea86ffce680916c3aec859be4a36c041/ap-201109140058034938868.jpg" width="88" height="130" alt="
+                New York Yankees closer Mariano Rivera throws against the Seattle Mariners in the ninth inning of a baseball game Tuesday, Sept. 13, 2011, in Seattle. The Yankees won 3-2. Rivera earned his 600th save, moving within one of Trevor Hoffman's major league record, by closing out the New York Yankees' 3-2 win over the Seattle Mariners on Tuesday night." align="left" border="0"></a></p>
+        <p>The celebration was pure Mariano Rivera. No big screams or wild gestures. Certainly no spectacle, even if Rivera is widely regarded as the best ever at what he does. Just warm hugs and appreciative handshakes from Yankees teammates. And even that might have been more than what Rivera wanted following the 600th save of his remarkable career.
+            <a href="http://us.rd.yahoo.com/sports/rss/top/SIG=11jrnp014/*http%3A//sports.yahoo.com/mlb/recap?gid=310913112">Read More</a>
+        </p><br clear="all" />
+        <p><i>Tuesday, 13th September 2011 11:35 pm</i></p>
+
+        <h2>Bowden: I had prostate cancer in 2007 (AP)</h2>
+        <p><a href="http://us.rd.yahoo.com/sports/rss/top/SIG=11sb0oir9/*http%3A//sports.yahoo.com/ncaaf/news?slug=ap-bowden-cancer"><img src="http://l.yimg.com/a/p/sp/tools/med/2011/09/ipt/1315914809.jpg" width="75" height="75" alt="Bobby Bowden" align="left" border="0"></a></p>
+        <p>Former Florida State football coach Bobby Bowden revealed he was successfully treated for prostate cancer in 2007, making the rounds Tuesday in New York to tell his story. Although he had kept it secret for more than four years, Bowden said he believed it was now \"my moral duty to bring it out in the open.\" Bowden, who turns 82 in early November, appeared on several morning television...
+            <a href="http://us.rd.yahoo.com/sports/rss/top/SIG=11sb0oir9/*http%3A//sports.yahoo.com/ncaaf/news?slug=ap-bowden-cancer">Read More</a>
+        </p><br clear="all" />
+        <p><i>Tuesday, 13th September 2011 2:20 pm</i></p>
+
+        <h2>Wakefield finally gets 200th win in rout (AP)</h2>
+        <p><a href="http://us.rd.yahoo.com/sports/rss/top/SIG=11jp8126r/*http%3A//sports.yahoo.com/mlb/recap?gid=310913102"><img src="http://d.yimg.com/a/p/sp/ap/9b/thumbe.ac31cb95b35206c55c99107c47fe7dde/ap-201109132158791188569.jpg" width="94" height="130" alt="Boston Red Sox starting pitcher Tim Wakefield laughs after being doused with champagne after recording his 200th career win during Boston's 18-6 win over the Toronto Blue Jays in a baseball game at Fenway Park in Boston on Tuesday, Sept. 13, 2011." align="left" border="0"></a></p>
+        <p>Fans chanted Tim Wakefield's name. A teammate sprayed him with champagne. It was about time. Baseball's oldest player earned his elusive 200th victory and the Boston Red Sox slugged their way to their first win in a week, 18-6 over the Toronto Blue Jays thanks to an 18-hit outburst. The 45-year-old knuckleballer was winless in his previous seven outings.
+        <a href="http://us.rd.yahoo.com/sports/rss/top/SIG=11jp8126r/*http%3A//sports.yahoo.com/mlb/recap?gid=310913102">Read More</a>
+        </p><br clear="all" />
+        <p><i>Tuesday, 13th September 2011 9:24 pm</i></p>
+
+        <h2>NCAA gives Boise State 3 years probation (AP)</h2>
+        <p>NCAA placed Boise State on probation for three years and imposed other sanctions Tuesday for major violations by the football program and other sports. The sanctions included a public reprimand, a one-year postseason ban for women's tennis and recruiting restrictions and scholarship reductions.<a href="http://us.rd.yahoo.com/sports/rss/top/SIG=11jp8126r/*http%3A//sports.yahoo.com/mlb/recap?gid=310913102">Read More</a></p>
+        <p><i>Tuesday, 13th September 2011 15:58:26 PDT</i></p>
+
+        <h2>Toyota taking orders in Japan for Prius Plug-in</h2>
+        <p><a href="http://news.yahoo.com/toyota-taking-orders-japan-prius-plug-041520935.html"><img src="http://l2.yimg.com/bt/api/res/1.2/E5AyIKzKHTtEgox_6phxxw--/YXBwaWQ9eW5ld3M7Zmk9ZmlsbDtoPTg2O3E9ODU7dz0xMzA-/http://media.zenfs.com/en_us/News/ap_webfeeds/7af0cd8ecd36d71aff0e6a706700bf00.jpg" alt="photo" align="left" title="Tokyo Motor Corp.'s Prius hybrid plug-in, the Prius PHV, is displayed at a press event in Tokyo Tuesday, Nov. 29, 2011. Toyota will begin taking orders Tuesday for the plug-in version of its hit Prius hybrid, announcing efficient mileage and a relatively affordable starting price of 3.2 million yen ($41,000), which comes down with green vehicle subsidies. (AP Photo/Itsuo Inouye)" border="0"></a> Toyota began taking orders Tuesday for the plug-in version of its hit Prius hybrid, announcing efficient mileage and a relatively affordable starting price of 3.2 million yen ($41,000), which comes down with green vehicle subsidies.
+            <a href="http://news.yahoo.com/toyota-taking-orders-japan-prius-plug-041520935.html">Read More</a>
+        </p><br clear="all" />
+        <p><i>Tuesday, 29 November 2011 6:16 am</i></p>
+
+        <h2>Steelers place starting RT Colon on IR (AP)</h2>
+        <p><a href="urn:newsml:sports.yahoo,ap:20050301:nfl,article,ap-steelers-colon:1">Willie Colon's season is over before it really started. Again. The Steelers placed their starting right tackle on the injured reserve list on Tuesday hours after the 28-year-old lineman underwent surgery to repair a torn right triceps sustained in a season-opening loss to Baltimore. This is the second straight year Colon's season has been cut remarkably short.</a></p>
+        <p><i>Tuesday, 13th September 2011 3:34 pm</i></p>
+
+
+    </div>
+
+
+</body>
+
+</html>
